1a9b79d3308c2e7e7f636cd7a2c2d21ce5519334
yvonneKim <yhk@utexas.edu>
2018-03-07 21:09:30 -0600

    Added some... sort of way to keep and print logic state

--- praetorian-mastermind.py
+++ praetorian-mastermind.py
@@ -26,8 +26,9 @@ def run(headers):
         
         win = False
         while(win == False):
-            g = guesser.next() 
-            print("TRYING GUESS: "+str(guesser))
+            g = guesser.next()
+            print("TABLE STATE: "+str(guesser))
+            print("TRYING GUESS: "+str(g))
             r = requests.post(levelurl, data=json.dumps(g), headers=h)
             print(r.json())
             if('message' in r.json()):
@@ -40,20 +41,49 @@ def run(headers):
 ### c(glads, weaps) * weaps! = total # of possibilities
 ### log(total#) = min number of steps to find answer
 # pass in json of the seed to init
-# for now, works only with level parameters
+# for now, works only with level 1 parameters
 class Guesser:
     def __init__(self, j):
-        self.glads = j['numGladiators']
-        self.guesses = j['numGuesses']
-        self.rounds = j['numRounds']
-        self.weaps = j['numWeapons']
-        self.guess = [0,1,2,3]
+        self.gl = j['numGladiators']
+        self.gs = j['numGuesses']
+        self.rn = j['numRounds']
+        self.wp = j['numWeapons']
+
+        # guess table keeps track of possibilities for each num's position
+        # each column is for each "weapon" number. it's rows have position numbers
+        # that are crossed off as we eliminate. -1 indicates a non-match.
+        #
+        #     -wp->
+        # |   0 , 1 , 2 , 3 , 4 , 5
+        # gl -----------------------
+        # v  -1  -1  -1  -1  -1  -1  [0]
+        #     0   0   0   0   0   0  [1]
+        #     1   1   1   1   1   1  [2]
+        #     2   2   2   2   2   2  [3]
+        #     3   3   3   3   3   3  [4]
+        #
+        # as we eliminate each number's possible positions, replace with "x"s.
+        # NOTE: the __str__() output of this table switches the axis :^/
+
+        self.gs_table = [[y-1 for y in range(self.gl+1)] for x in range(self.wp)]        
         
     def __str__(self):
-        return ' '.join(str(self.guess))
+        out = "\n"
+        i = 0
+        for col in self.gs_table:
+            out += str(i)+" | "
+            i += 1
+            for val in col:
+                out += str(val)+" "
+            out += '\n'
+        return out
         
     def next(self):
-        # for now, returns the same thing over and over
+        # returns next guess (as dict, to be json-ified) from the last
+        # state of the gs_table
+        # first time running next() gives a defaulto [0, 1, 2, 3] guess
+
+        
         return {'guess':[0,1,2,3]}
 
     
